b752d7ce4681646c64559997f6d07daa2e86d79a
chakravarthigiduthuri <chakravarthig99@gmail.com>
2021-01-14 16:33:23 +0530

    intial commit

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showinfo, setShowinfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowinfo(!showinfo)}>
+          {showinfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showinfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
